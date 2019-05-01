270e63ae2a09f9c7967e1db8f72842f3fca39837
Murillo Nunes <murillo.nns@gmail.com>
2019-05-01 18:12:42 -0300

    calculadora-imc: exercício finalizado

--- calculadora-imc/index.js
+++ calculadora-imc/index.js
@@ -1,10 +1,34 @@
-function calcularIMC(){
+function calcularIMC() {
   var formulario = document.getElementById("formulario");  
   
   var massa = +formulario.massa.value;
   var altura = +formulario.altura.value;
-  
+
+  altura = altura / 100;
   var imc = massa / (altura * altura);
   
   formulario.imc.value = imc.toFixed(2);
+
+  classificarIMC();
+}
+
+function classificarIMC() {
+  var imc = formulario.imc.value;
+
+  if (imc < 18.5) {
+    var classificacao = "Abaixo do peso";
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    var classificacao = "Peso normal";
+  } else if (imc >= 25 && imc <= 29.9) {
+    var classificacao = "Sobrepeso";
+  } else if (imc >= 30 && imc <= 34.9) {
+    var classificacao = "Obesidade grau 1";
+  } else if (imc >= 35 && imc <= 39.9) {
+    var classificacao = "Obesidade grau 2";
+  } else {
+    var classificacao = "Obesidade grau 3";
+  }
+
+  formulario.classificacao.value = classificacao;
+
 }
